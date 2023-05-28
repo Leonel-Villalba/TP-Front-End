11a06fce0c3d763d35c45ff78da67ded02387116
milagros gomez <leo.villabaa13@gmail.com>
2023-05-28 19:15:42 -0300

    prueba js

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ const enlaces = document.querySelector('.nav-items');
 ham.addEventListener('click', () => {
     enlaces.classList.toggle('activado');
 })
+
+
+const cantidad = document.getElementById('cant').value;
+const porcDescuento = document.getElementById("porcDescuento").value;
+console.log("porcDesc: " + porcDescuento);
+
